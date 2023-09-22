8322d64c55a3aafbd4b21c55e990ae634b4f8bc5
Kelvem Ferreira <124929808+Kelvem01@users.noreply.github.com>
2023-09-22 16:12:05 -0300

    função que soma no sql

--- funcoes_teste.py
+++ funcoes_teste.py
@@ -127,37 +127,35 @@ def exemplo_joins4(conn):
 	#		ON MateriaisEquipamentosProcedimento.Procedimento_id = Procedimento.id;
 	#	"""
 
-	comando = f"""
-		SELECT
-			MateriaisEquipamentosProcedimento.MateriaisEquipamentos_id,
-			MateriaisEquipamentos.id,
-			MateriaisEquipamentos.tipo,
-			MateriaisEquipamentosProcedimento.qtd_utilizada,
-			MateriaisEquipamentos.valor,
-			MateriaisEquipamentosProcedimento.Procedimento_id,
-			Procedimento.id,
-			Procedimento.tipo,
-			Procedimento.cliente_id,
-			SUM(MateriaisEquipamentosProcedimento.qtd_utilizada*MateriaisEquipamentos.valor)
-
-		FROM MateriaisEquipamentosProcedimento
-		INNER JOIN MateriaisEquipamentos
-			ON MateriaisEquipamentosProcedimento.MateriaisEquipamentos_id = MateriaisEquipamentos.id
-		INNER JOIN Procedimento
-			ON MateriaisEquipamentosProcedimento.Procedimento_id = Procedimento.id
-		GROUP BY Procedimento.id;
-	"""
-
-	cursor.execute(comando)  ## Realizar uma nova Checagem no sql para realizar a contabilidade do procedimento com os materiais,
-	dados = cursor.fetchall()
-	for dado in dados:
-    		print(f"""
-            ID:{dado[0]} |Tipo Material:{dado[2]}
-            Quantidade Utilizada:{dado[3]}
-            Valor Mateial : R$ {dado[4]}
-            ID Procedimento:{dado[6]}| Procedimento :{dado[7]} | ID Clinte :{dado[8]} 
-            Total:R$ {dado[9]}
-            """)
+	#comando = f"""
+	#	SELECT
+	#		MateriaisEquipamentosProcedimento.MateriaisEquipamentos_id,
+	#		MateriaisEquipamentosProcedimento.qtd_utilizada,
+	#		MateriaisEquipamentos.valor,
+	#		MateriaisEquipamentosProcedimento.Procedimento_id,
+	#		Procedimento.id,
+	#		Procedimento.tipo,
+	#		Procedimento.cliente_id,
+	#		SUM(MateriaisEquipamentosProcedimento.qtd_utilizada*MateriaisEquipamentos.valor)
+    #
+	#	FROM MateriaisEquipamentosProcedimento
+	#	INNER JOIN MateriaisEquipamentos
+	#		ON MateriaisEquipamentosProcedimento.MateriaisEquipamentos_id = MateriaisEquipamentos.id
+	#	INNER JOIN Procedimento
+	#		ON MateriaisEquipamentosProcedimento.Procedimento_id = Procedimento.id
+	#	GROUP BY Procedimento.id;
+	#"""
+	#
+	#cursor.execute(comando)  ## Realizar uma nova Checagem no sql para realizar a contabilidade do procedimento com os materiais,
+	#dados = cursor.fetchall()
+	#for dado in dados:
+    #		print(f"""
+     #       ID:{dado[0]} |Tipo Material:{dado[2]}
+      ##      Quantidade Utilizada:{dado[3]}
+       #     Valor Mateial : R$ {dado[4]}
+        #    ID Procedimento:{dado[6]}| Procedimento :{dado[7]} | ID Clinte :{dado[8]} 
+         #   Total:R$ {dado[9]}
+         #   """)
 		#print(dado)
 		#print(f""" Id Cliente: {dado[8]}
         #Id material: {dado[1]} | Material: {dado[2]}
@@ -165,7 +163,35 @@ def exemplo_joins4(conn):
 		#Valor: {dado[4]}
 		#Id procedimento: {dado[6]} | Procedimento: {dado[7]}
     	#""") #Mostra a tupla recuperada do BD
+############################################################################## opção com o nome do cliente
+	comando = """
+    SELECT 
+        Cliente.cliente_id,
+        Cliente.nome AS nome_cliente,
+        Procedimento.id AS id_procedimento,
+        Procedimento.tipo AS tipo_procedimento,
+        Cirurgiao.id AS id_cirurgiao,
+        Cirurgiao.nome AS nome_cirurgiao,
+        MateriaisEquipamentosProcedimento.Procedimento_id AS id_procedimento_material,
+        SUM(MateriaisEquipamentosProcedimento.qtd_utilizada * MateriaisEquipamentos.valor) AS custo_total_material
+    FROM Cliente
+    INNER JOIN Procedimento ON Cliente.Procedimento_id = Procedimento.id
+    INNER JOIN Cirurgiao ON Procedimento.Cirurgiao_id = Cirurgiao.id
+    INNER JOIN MateriaisEquipamentosProcedimento ON Procedimento.id = MateriaisEquipamentosProcedimento.Procedimento_id
+    INNER JOIN MateriaisEquipamentos ON MateriaisEquipamentosProcedimento.MateriaisEquipamentos_id = MateriaisEquipamentos.id
+    GROUP BY Procedimento.id, Cliente.cliente_id, Cliente.nome, Procedimento.id, Procedimento.tipo, Cirurgiao.id, Cirurgiao.nome, MateriaisEquipamentosProcedimento.Procedimento_id;
+"""
 
+	
+	cursor.execute(comando)  ## Realizar uma nova Checagem no sql para realizar a contabilidade do procedimento com os materiais,
+	dados = cursor.fetchall()
+	for dado in dados:
+		print(dado)
+     
+     
+     
+     
+    ###############################################################################################################
 #Calcula o valor TOTAL por CLIENTE (TODOS OS PROCEDIMENTOS) | Obs.: É necessário melhorar a consulta para evitar pagamentos DUPLICADOS,
 # mantendo apenas a contabilização de pagamentos NÃO EFETUADOS.
 def exemplo_joins5(conn):
